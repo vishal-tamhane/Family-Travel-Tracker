645ca2c7342ec816bf3f9347a1f0520a01684916
vishal-Tamhane <vishaltamhane367@gmail.com>
2025-09-01 17:55:55 +0530

    Refactor database connection to use environment variables for improved security

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "world",
-  password: "vishal@9699#",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 db.connect();
 
